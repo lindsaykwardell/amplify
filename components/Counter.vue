0149be1c3e16f11b9705e53c9f60b5edff7d66d5
Lindsay Wardell <three060@gmail.com>
2024-05-29 10:51:03 -0700

    Got tooling set up

---
+++ components/Counter.vue
@@ -0,0 +1,10 @@
+<template>
+  <ElmCounter />
+</template>
+
+<script setup lang="ts">
+import { Elm } from "~/src/Main.elm";
+import elmBridge from "elm-vue-bridge";
+
+const ElmCounter = elmBridge(Elm, "Counter");
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import elmPlugin from "vite-plugin-elm";
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  vite: {
+    plugins: [elmPlugin()],
+  },
+});
