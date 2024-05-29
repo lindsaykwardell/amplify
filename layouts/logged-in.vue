bb912c80aa98b3959bbd29594cd9f2bfd3373367
Lindsay Wardell <three060@gmail.com>
2024-05-29 15:13:18 -0700

    Final stuff

--- layouts/logged-in.vue
+++ layouts/logged-in.vue
@@ -5,6 +5,7 @@
       <div class="text-2xl font-raleway italic">Dysentery</div>
       <div class="flex gap-4 justify-end flex-grow p-2">
         <NuxtLink to="/dashboard">Dashboard</NuxtLink>
+        <button @click="initSignOut">Sign out</button>
       </div>
     </nav>
     <main>
@@ -12,3 +13,13 @@
     </main>
   </div>
 </template>
+
+<script setup lang="ts">
+const router = useRouter();
+const signOut = useNuxtApp().$Amplify.Auth.signOut;
+
+function initSignOut() {
+  signOut();
+  router.push("/");
+}
+</script>
